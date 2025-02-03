1039dafc236598f0c94a583d1d0add092b61e369
Rendy Yofana Kusuma <rendyyofanakusuma@gmail.com>
2025-02-03 19:45:04 +0700

    Add require DB

--- app/Http/Controllers/AjuanController.php
+++ app/Http/Controllers/AjuanController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\PengajuanPending;
 use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Support\Facades\DB;
 
 class AjuanController extends Controller
 {
